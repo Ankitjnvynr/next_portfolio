b9aca440eb448fb698fd38cca307dc91d34747dd
ankitjnvynr <ankitbkana@outlook.com>
2024-07-18 11:17:21 +0530

    motion graphic reels page added content

--- src/MotionGraphics.jsx
+++ src/MotionGraphics.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import MotionGraphicsReel from './components/workItems/MotionGraphicsReel';
 
 function MotionGraphics() {
+
+  const MotionGraphicsReels = {
+    vid1: '/videos/Gita jeevan geet motion graphics.mp4',
+    vid2: '/videos/divy gita satsang motion graphics.mp4',
+    vid3: '/videos/divy gita satsang motion graphics video.mp4',
+    vid4: '/videos/Gita jeevan geet motion graphics.mp4',
+    vid5: '/videos/Gita jeevan geet motion graphics.mp4',
+    vid6: '/videos/Gita jeevan geet motion graphics.mp4',
+  };
   return (
-    <div>MotionGraphics</div>
+    <div>
+        <h2 className="text-2xl">Motion Graphics:- </h2>
+        <p className="text-xl text-slate-600 text-justify">
+          To creating Motion Graphics or animations, I mostly use Adobe After
+          Effects.
+        </p>
+        <div className="flex flex-wrap gap-2 my-3 justify-between">
+          {Object.entries(MotionGraphicsReels).map(([key, src], index) => {
+            if (index > 3) return;
+            return <MotionGraphicsReel key={key} src={src} />;
+          })}
+        </div>
+        
+          
+            
+          
+        
+      </div>
   )
 }
 
--- src/app/motion-graphics/page.js
+++ src/app/motion-graphics/page.js
@@ -1,9 +1,10 @@
+import MotionGraphics from '@/MotionGraphics'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className="max-w-[1300px] w-[95%] m-auto bg-white shadow-lg my-4 rounded-xl overflow-hidden p-5">
+      <MotionGraphics />
     </div>
   )
 }
